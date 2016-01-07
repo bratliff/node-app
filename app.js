edaaf87a866d5ab1babdd280afd243109d26d005
Benjamin Ratliff <ben@2bitcreative.com>
2016-01-07 16:03:32 -0500

    corrected the server start

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get("/dog", function(req, res) {
 	res.send('Meow!');
 });
 
-app.listen(process.env.PORT, process.env.IP function(){
+app.listen(process.env.PORT, process.env.IP, function(){
 	console.log('server has started');
 });
 
